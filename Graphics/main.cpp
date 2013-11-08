f781a81c6faf2ef05c546c8718d54a435cf0c757
DarthGibblet <DarthGibblet@gmail.com>
2013-11-07 23:17:14 -0600

    Added a texture
    Added a texture resource and bare-bones code to load .dds textures into
    OpenGL

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include <Windows.h>
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -22,6 +24,8 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	glEnable(GL_TEXTURE_2D);
+
 	GLuint shaderVP = glCreateShader(GL_VERTEX_SHADER);
 	GLuint shaderFP = glCreateShader(GL_FRAGMENT_SHADER);
 
@@ -39,7 +43,74 @@ int main(int argc, char** argv)
 	glAttachShader(shaderID, shaderVP);
 	glLinkProgram(shaderID);
 
-	glUseProgram(shaderID);
+	//glUseProgram(shaderID);
+
+	unsigned char header[124];
+
+	FILE *fp;
+
+	fopen_s(&fp, "..\\resources\\Gust.dds", "rb");
+
+	char filecode[4];
+	fread(filecode, 1, 4, fp);
+	if(strncmp(filecode, "DDS ", 4) != 0)
+	{
+		fclose(fp);
+		return 0;
+	}
+
+	fread(&header, 124, 1, fp);
+	unsigned int height = *(unsigned int*)&(header[8]);
+	unsigned int width = *(unsigned int*)&(header[12]);
+	unsigned int linearSize = *(unsigned int*)&(header[16]);
+	unsigned int mipMapCount = *(unsigned int*)&(header[24]);
+	unsigned int fourCC = *(unsigned int*)&(header[80]);
+
+	unsigned char* buffer;
+	unsigned int bufsize;
+	bufsize = mipMapCount > 1 ? linearSize * 2 : linearSize;
+	buffer = (unsigned char*)malloc(bufsize * sizeof(unsigned char));
+	auto rv = fread(buffer, 1, bufsize, fp);
+	fclose(fp);
+
+	unsigned int components = (fourCC == mmioFOURCC('D', 'X', 'T', '1'));
+	unsigned int format;
+	switch(fourCC)
+	{
+	case mmioFOURCC('D','X','T','1'):
+		format = GL_COMPRESSED_RGBA_S3TC_DXT1_EXT;
+		break;
+	case mmioFOURCC('D','X','T','3'):
+		format = GL_COMPRESSED_RGBA_S3TC_DXT3_EXT;
+		break;
+	case mmioFOURCC('D','X','T','5'):
+		format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+		break;
+	default:
+		free(buffer);
+		return 0;
+	}
+
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+
+	glBindTexture(GL_TEXTURE_2D, textureID);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+	unsigned int blockSize = (format == GL_COMPRESSED_RGBA_S3TC_DXT1_EXT) ? 8 : 16;
+	unsigned int offset = 0;
+
+	for(unsigned int level = 0; level < mipMapCount && (width || height); ++level)
+	{
+		unsigned int size = ((width + 3)/4) * ((height + 3)/4)*blockSize;
+		glCompressedTexImage2D(GL_TEXTURE_2D, level, format, width, height, 0, size, buffer + offset);
+		offset += size;
+		width /= 2;
+		height /= 2;
+	}
+	free(buffer);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -62,12 +133,16 @@ int main(int argc, char** argv)
 		glRotatef((float) glfwGetTime() * 50, 0, 0, 1);
 
 		glBegin(GL_QUADS);
+		glTexCoord2f(0, 0);
 		glColor3f(1, 0, 0);
 		glVertex3f(-0.6f, -0.6f, 0);
+		glTexCoord2f(1, 0);
 		glColor3f(0, 1, 0);
 		glVertex3f(0.6f, -0.6f, 0);
+		glTexCoord2f(1, 1);
 		glColor3f(0, 0, 1);
 		glVertex3f(0.6f, 0.6f, 0);
+		glTexCoord2f(0, 1);
 		glColor3f(1, 1, 0);
 		glVertex3f(-0.6f, 0.6f, 0);
 		glEnd();
